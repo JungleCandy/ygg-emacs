7d1a347bf79aa0b42e188e455bad7776fc768805
Abizer Nasir <abizern@mac.com>
2022-12-04 12:15:10 +0000

    Add clj-refactor package

--- init.el
+++ init.el
@@ -701,6 +701,15 @@
 (use-package cider
   :ensure t)
 
+(defun ygg-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(use-package clj-refactor
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'ygg-clojure-mode-hook))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
