0ed58b495c44781c7b26ebf6bbc1a8e3e218e5c3
Abizer Nasir <abizern@mac.com>
2024-12-31 15:31:32 +0000

    partila

--- init.el
+++ init.el
@@ -3,20 +3,29 @@
 
 ;;; init.el --- My personal configuration file.
 
+;; Reduces the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB).
+(setq gc-cons-threshold 50000000)
+
+;; Warn when opening files bigger than 100MB.
+(setq large-file-warning-threshold 100000000)
+
 (require 'package)
 
 ;; Keep downloaded packages organised.
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(setq package-user-dir
+      (expand-file-name "elpa"
+                        user-emacs-directory))
 
 
 ;; Set priorities for which archives are used.
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+(setq package-archives '(("melpa"  . "https://melpa.org/packages/")
                          ("stable" . "https://stable.melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")))
+                         ("gnu"    . "https://elpa.gnu.org/packages/")
+                         ("org"    . "http://orgmode.org/elpa/")))
 (customize-set-variable 'package-archive-priorities '(("gnu"    . 63)
                                                       ("nongnu" . 10)
-                                                      ("org" . 99)
+                                                      ("org"    . 99)
                                                       ("stable" . 80)
                                                       ("melpa"  . 90)))
 
